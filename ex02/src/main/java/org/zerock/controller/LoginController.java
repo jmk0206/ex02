b87948a0560727be3b6883c0766a456e502dc8dc
jmk0206 <jmk960206@gmail.com>
2021-08-31 12:49:46 +0900

    로그인 만들기

---
+++ ex02/src/main/java/org/zerock/controller/LoginController.java
@@ -0,0 +1,30 @@
+package org.zerock.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("login")
+public class LoginController {
+	private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
+	
+	@GetMapping("all")
+	public void doAll() {
+		logger.info("do all can access everybody");
+	}
+	
+	@GetMapping("member")
+	public void doMember() {
+		logger.info("logined member");
+	}
+	
+	@GetMapping("admin")
+	public void doAdmin() {
+		logger.info("admin only");
+	}
+
+	
+ }
